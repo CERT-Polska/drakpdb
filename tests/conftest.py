2ec4513683195be7dece98a9b0ed805fad819ce2
Paweł Srokosz <psrok1@gmail.com>
2024-07-30 18:31:41 +0200

    Tests and small after-test bugfixes and improvements (#4)
    * Tests and small after-test bugfixes and improvements
    
    * More tests

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pathlib
+
+package_dir = pathlib.Path(__file__).parent.absolute()
+pdbs_dir = package_dir / "pdbs"
+
+
+def pytest_generate_tests(metafunc):
+    if 'pdb_file' in metafunc.fixturenames:
+        pdbs_files = list(pdbs_dir.glob("*.pdb"))
+        metafunc.parametrize('pdb_file', pdbs_files)
